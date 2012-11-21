1c953121e29126de54d009ae7594832f44823529
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2012-11-21 21:17:44 +0100

    Add the forms library from other project.

---
+++ forms/form.go
@@ -0,0 +1,129 @@
+package forms
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/ernestokarim/gaelib/app"
+)
+
+type Field interface {
+	Build() string
+}
+
+type Form struct {
+	Name           string
+	Method, Action string
+	FieldNames     []string
+	Fields         map[string]Field
+}
+
+func New(action string) *Form {
+	return &Form{
+		Method:     "POST",
+		Action:     action,
+		FieldNames: make([]string, 0),
+		Fields:     make(map[string]Field),
+	}
+}
+
+func (f *Form) AddField(name string, field Field) {
+	f.FieldNames = append(f.FieldNames, name)
+	f.Fields[name] = field
+}
+
+func (f *Form) Build() string {
+	// Set the error class if needed
+	out := ""
+	for _, name := range f.FieldNames {
+		out += f.Fields[name].Build()
+	}
+
+	// Set the form legend if needed
+	legend := ""
+	if f.Name != "" {
+		legend = "<legend>" + f.Name + "</legend>"
+	}
+
+	return fmt.Sprintf(`
+		<form action="%s" method="%s" class="form-horizontal">
+			<fieldset>%s%s</fieldset>
+		</form>
+	`, f.Action, f.Method, legend, out)
+}
+
+func (f *Form) Validate(r *app.Request, data interface{}) (bool, error) {
+	if err := r.Req.ParseForm(); err != nil {
+		return false, app.Error(err)
+	}
+
+	failed := false
+	for _, field := range f.Fields {
+		if control := getControl(field); control != nil {
+			// Extract the control value and assign it
+			value := normalizeValue(control.Id, r.Req.Form)
+			control.Value = value
+
+			// Run each validation for this field
+			for _, val := range control.Validations {
+				if err := val.Func(value); err != "" {
+					failed = true
+
+					control.Error = err
+					if control.ResetValue {
+						control.Value = ""
+					}
+
+					break
+				}
+			}
+		}
+	}
+
+	if !failed {
+		if err := r.LoadData(data); err != nil {
+			return true, err
+		}
+	}
+
+	return !failed, nil
+}
+
+func getControl(f Field) *Control {
+	// Control for inputs
+	input, ok := f.(*InputField)
+	if ok {
+		return input.Control
+	}
+
+	// Control for selects
+	sel, ok := f.(*SelectField)
+	if ok {
+		return sel.Control
+	}
+
+	// Control for textarea
+	textarea, ok := f.(*TextAreaField)
+	if ok {
+		return textarea.Control
+	}
+
+	// Not a control
+	return nil
+}
+
+func normalizeValue(id string, v url.Values) string {
+	// Extract the value
+	values, ok := v[id]
+	if ok {
+		// Trim the value
+		v[id] = []string{strings.TrimSpace(values[0])}
+
+		// Return the value
+		return v[id][0]
+	}
+
+	// No value found
+	return ""
+}
