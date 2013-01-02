92e3b486644c3aefb633a5d1a719992c13ae4438
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-01-02 11:35:59 +0100

    Add utils. Transfer the methods of model to global functions.

--- db/entity.go
+++ db/entity.go
@@ -12,6 +12,7 @@ import (
 type Entity interface {
 	Key() *datastore.Key
 	SetKey(*datastore.Key)
+	AddKey(*datastore.Key)
 }
 
 func Kind(kind string) *datastore.Query {
@@ -27,6 +28,38 @@ func Query(c appengine.Context, q *datastore.Query, results interface{}) error {
 	v := reflect.ValueOf(results).Elem()
 	for i, key := range keys {
 		v.Index(i).Interface().(Entity).SetKey(key)
+	}
+
+	return nil
+}
+
+func Get(c appengine.Context, key *datastore.Key, result interface{}) (bool, error) {
+	if err := datastore.Get(c, key, result); err != nil {
+		if err == datastore.ErrNoSuchEntity {
+			return false, nil
+		}
+		return false, app.Error(err)
+	}
+
+	result.(Entity).SetKey(key)
+
+	return true, nil
+}
+
+func Put(c appengine.Context, data interface{}) error {
+	ent := data.(Entity)
+	key, err := datastore.Put(c, ent.Key(), data)
+	if err != nil {
+		return app.Error(err)
+	}
+	ent.SetKey(key)
+
+	return nil
+}
+
+func Delete(c appengine.Context, key *datastore.Key) error {
+	if err := datastore.Delete(c, key); err != nil {
+		return app.Error(err)
 	}
 
 	return nil
--- db/model.go
+++ db/model.go
@@ -1,9 +1,6 @@
 package db
 
 import (
-	"github.com/ernestokarim/gaelib/app"
-
-	"appengine"
 	"appengine/datastore"
 )
 
@@ -19,36 +16,8 @@ func (m *Model) SetKey(key *datastore.Key) {
 	m.key = key
 }
 
-func (m *Model) GetOrDefault(c appengine.Context, result interface{}) error {
+func (m *Model) AddKey(key *datastore.Key) {
 	if m.key == nil {
-		panic("no key to get the model")
+		m.key = key
 	}
-
-	if err := datastore.Get(c, m.key, result); err != nil {
-		if err == datastore.ErrNoSuchEntity {
-			return nil
-		}
-		return app.Error(err)
-	}
-
-	return nil
-}
-
-func (m *Model) Save(c appengine.Context, data interface{}) error {
-	key, err := datastore.Put(c, m.key, data)
-	if err != nil {
-		return app.Error(err)
-	}
-
-	m.key = key
-
-	return nil
-}
-
-func (m *Model) Delete(c appengine.Context) error {
-	if err := datastore.Delete(c, m.key); err != nil {
-		return app.Error(err)
-	}
-
-	return nil
 }
